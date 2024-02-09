01c155994b5f3a9f116aa09788135ba00b674a2c
Kushal Matalia <kushalmatalia15.12@gmail.com>
2024-02-09 11:15:06 +0530

    Adding comments and documentations for better understanding - v1

--- detection_counter.py
+++ detection_counter.py
@@ -5,20 +5,26 @@ import pandas as pd
 from ultralytics import YOLO
 from utils import putTextRect as ptr
 
+# Loading the polyCords file, consisting all the saved coordinates of the RoIs
+# Loading the polylines list data and the area_name data
 with open("polyCords","rb") as f:
         data = pickle.load(f)
         polylines,area_name = data['polylines'], data['area_name']
 
+# Loading coco.txt data, COCO i.e. the Common Objects in Context, has by default 80 classes of objects pre-trained
 my_file = open("coco.txt", "r")
 data = my_file.read()
 class_list = data.split("\n")
 
+# Loading the YOLO v8 model for Object Detection
 model=YOLO('yolov8s.pt')
 
-cap=cv2.VideoCapture('')
+# Loading the video feed - Plaksha_Parking2 to be used for Plaksha
+cap=cv2.VideoCapture('Plaksha_Parking2.mp4')
 
 count=0
 
+# Reading the video feed frame by frame, and when completed, set to the beginning to again start the loop
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -29,29 +35,38 @@ while True:
     if count % 3 != 0:
        continue
 
+    # Setting frame size to 1020x500
     frame=cv2.resize(frame,(1020,500))
     frame_copy = frame.copy()
-    results=model.predict(frame)
- #   print(results)
-    a=results[0].boxes.data
-    px=pd.DataFrame(a).astype("float")
-#    print(px)
+
+    # Predicting the YOLO model on the every fram of the video feed
+    # YOLO model is trained on the COCO dataset, which has the data of all types of vehicles required
+    results = model.predict(frame)
+
+    # Fetching the bounding boxes from the prediction results
+    # Extracting bounding box coordinates and class predictions.
+    a = results[0].boxes.data
+    px = pd.DataFrame(a).astype("float")
     
     list1 = []
     for index,row in px.iterrows():
-#        print(row)
- 
-        x1=int(row[0])
-        y1=int(row[1])
-        x2=int(row[2])
-        y2=int(row[3])
-        d=int(row[5])
-        
-        c=class_list[d]
-        # print("Object identified as -> ",class_list[d])
-        cx=int(x1+x2)//2
-        cy=int(y1+y2)//2
 
+        # Storing all the coordinates of the bounding box formed and the class information detected
+        x1 = int(row[0])
+        y1 = int(row[1])
+        x2 = int(row[2])
+        y2 = int(row[3])
+        d = int(row[5])
+        
+        c = class_list[d]
+        # print("Object identified as -> ",class_list[d])
+
+        # Finding the middle point of the bounding box, required to do further calculations
+        cx = int(x1+x2)//2
+        cy = int(y1+y2)//2
+
+        # Filtering the vehicles required to be detected and appending in the list
+        # Can uncomment the code line to form the bounding box created around the detected object, here vehicles filterd above
         if 'car' in c or 'cars' in c or 'truck' in c or 'trucks' in c or 'bus' in c or 'buss' in c:
             list1.append([cx,cy])
             # cv2.rectangle(frame,(x1,y1),(x2,y2),(255,255,255),2)
@@ -60,31 +75,39 @@ while True:
     list2 = []
     empty_spaces = ""
 
+    # By default, all the RoIs are marked as green colors i.e the polygons
     for i, polyline in enumerate(polylines):
         cv2.polylines(frame, [polyline], True, (0, 255, 0), 2)
         ptr(frame, f'{area_name[i]}', tuple(polyline[0]), 1, 1)
 
         area_empty = True
 
+        # Testing for the center point caclulated above to be present within the polygon i.e the RoIs
         for j, centroid in enumerate(list1):
             cx1 = centroid[0]
             cy1 = centroid[1]
             result = cv2.pointPolygonTest(polyline, ((cx1, cy1)), False)
 
+            # The center points of all the bounding boxes of the detected vehicle are marked by blue dot
+            # Code block to filter out the available i.e. the free spots among all the spots
             if result >= 0:
                 cv2.circle(frame, (cx1, cy1), 3, (255, 0, 0), -1)
+
+                # If the object is present, the RoIs are marked by Red color
                 cv2.polylines(frame, [polyline], True, (0, 0, 255), 2)
                 counter.append(cx1)
                 list2.append(i)
                 area_empty = False
                 break
         
+        # Is the area_empty is set to True i.e. the RoI is empty 
         if area_empty:
             print("Area", area_name[i], "is empty.")
             empty_spaces += area_name[i] + ", "
 
         # ptr(frame, f'CarCounter: - {empty_spaces}, (50, 150 + i * 40), 2, 2)
 
+    # Code block to create the rectangle box to display the counter values
     car_count = len(counter)
     free_space = len(polylines) - len(list2)
     ptr(frame, f'Occupied: - {car_count}', (50, 60), 2, 2)
@@ -94,6 +117,11 @@ while True:
     cv2.imshow('FRAME', frame)
     key = cv2.waitKey(1) & 0xFF
 
+    # Keyboard event, where:
+
+
+    
+    # key 'q' -> to stop the video feed and exit
     if key == ord('q'):
         break
 
--- roi_train.py
+++ roi_train.py
@@ -3,12 +3,19 @@ import numpy as np
 from utils import putTextRect as ptr
 import pickle
 
+# Loading the video feed - Plaksha_Parking2 to be used for Plaksha
 cap = cv2.VideoCapture('')
 
+# drawing - boolean to be true/false to decide wheather the current mouse event is eligible for drawing or stop drawing
+# area_name - the list to store all the area names for all the polygons drawn i.e. the RoIs
+# paused - booelan to be used to pause the video feed as soon as the mouse event is triggered
 drawing = False
 area_name = []
 paused = False
 
+# Try-Except block to read the polyCord file to read the coordinates for all the polygons drawn : If Exists
+# or else, set the polylines list to be empty
+# polylines - list to store arrays of polygon coordinates 
 try:
     with open("polyCords","rb") as f:
                 data = pickle.load(f)
@@ -16,50 +23,61 @@ try:
 except:
     polylines = []
 
+# points - list to store all the individual points
+# current_name - the current name of the area under consideration
 points = []
 current_name = " "
 
+# Code block to draw the polylines and create the RoIs i.e. the bounding boxes for each parking spots
+# Hardware inputs - mouse left and right buttons
 def draw(event, x, y, flags, param):
     global points, drawing, paused
     drawing = True
 
+    # Left Mouse Button event
+    # Drawing polygons and creating the RoIs
+    # Storing the initial coordinates of the mouse click to start the polyline
     if event==cv2.EVENT_LBUTTONDOWN:
-        # print(x, y)
         points=[(x,y)]
         paused = not paused
 
+    # Storing all the points while the mouse is dragged to draw the polygon
     elif event==cv2.EVENT_MOUSEMOVE:
         if drawing:
             points.append((x,y))
 
+    # Storing all the polygon coordinates along with the area name (which is generated automatically - 1,2,...) while the mouse button is released
     elif event==cv2.EVENT_LBUTTONUP:
         drawing = False
         current_name = str(len(area_name) + 1)
-        # current_name = input('areaname: - ')
-        # if current_name:
         area_name.append(current_name)
         polylines.append(np.array(points,np.int32))
-        # print("polylines ---> ", len(polylines))
-        # print("points ---> ", len(points))
         paused = not paused
 
+    # Right Mouse Button event
+    # Removing the polygons and the RoIs
     elif event == cv2.EVENT_RBUTTONDOWN:
-        # remove_last_polyline()
+        # Based on the True/False value the respective methods are called to remove the polygon
         if not is_inside_polyline(x, y):
             remove_last_polyline()
         else:
             remove_polyline_at_point(x, y)
 
+# When the right button is clicked anywhere else other than the RoIs:
+# Removing the last created polygon
 def remove_last_polyline():
     if polylines:
         del polylines[-1]
         if area_name:
             del area_name[-1]
 
+# When the right button is clicked anywhere within the RoIs:
+# Removing that particular polygon, and accordingly update the area names
 def remove_polyline_at_point(x, y):
     global area_name, polylines
     removed_index = None
 
+    # Testing whether the coordinates of the point clicked is within the polygon - using pointPolygonTest
     for i, polyline in enumerate(polylines):
         result = cv2.pointPolygonTest(polyline, (x, y), False)
         if result >= 0:
@@ -74,6 +92,7 @@ def remove_polyline_at_point(x, y):
         for i in range(removed_index, len(area_name)):
             area_name[i] = str(int(area_name[i]) - 1)
 
+# Method to return True/False if the coordinate of the point of the button clicked is within any RoIs
 def is_inside_polyline(x, y):
     for polyline in polylines:
         result = cv2.pointPolygonTest(polyline, (x, y), False)
@@ -81,6 +100,7 @@ def is_inside_polyline(x, y):
             return True
     return False
 
+# Reading the video feed frame by frame, and when completed, set to the beginning to again start the loop
 while True:
     if not paused:
         ret, frame = cap.read()
@@ -88,8 +108,11 @@ while True:
             cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
             continue
 
+    # Setting frame size to 1020x500
     frame=cv2.resize(frame,(1020,500))
 
+    # Drawing the polygons i.e. the RoIs, which are red in color
+    # Appending the area names by default as numbers
     for i, polyline in enumerate(polylines):
         print("testing polyline enumeratte --> ", i)
         cv2.polylines(frame,[polyline], True, (0,0,255), 2)
@@ -99,6 +122,9 @@ while True:
     cv2.setMouseCallback('FRAME', draw)
     Key = cv2.waitKey(10) & 0xFF
 
+    # Keyboard event, where:
+    # key 's' -> to save all the polylines drawn i.e. the polygons i.e. the RoIs at any moment in time
+    # key 'q' -> to stop the video feed and exit
     if Key == ord('s'):
         with open("polyCords","wb") as f:
             data = {
